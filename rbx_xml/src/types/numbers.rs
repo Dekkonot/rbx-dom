d1433ee1583d0c5e914945758583fc9e37b733d1
Dekkonot <git@dekkonot.com>
2023-10-13 18:01:08 -0700

    Fix number test

--- rbx_xml/src/types/numbers.rs
+++ rbx_xml/src/types/numbers.rs
@@ -112,7 +112,6 @@ mod test {
         // using as_bytes.
         #[allow(clippy::string_lit_as_bytes)]
         let mut reader = XmlEventReader::from_source(r#"<float name="foo">NAN</float>"#.as_bytes());
-        reader.next().unwrap().unwrap(); // Eat StartDocument event
         let value = f32::read_outer_xml(&mut reader).unwrap();
         assert!(value.is_nan());
     }
